0b1ce40c73805df3c64572f9fca04bf26208f172
Raghuvaran Alagarsami <raghuvaran.alagarsami@tui.com>
2025-08-03 14:04:07 +0530

    removed main

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 type Shape interface {
 	Area() float64
@@ -17,14 +15,20 @@ type Circle struct {
 	radius float64
 }
 
-func (s Square) Area() float64 { return s.side * s.side }
-func (c Circle) Area() float64 { return 3.14 * c.radius * c.radius }
-
-func main() {
-	fmt.Println("Hello Welcome to this printInterface main function")
-	val := Square{side: 10.0}
-	fmt.Println("square val:", val.Area())
-
-	cval := Circle{radius: 5.0}
-	fmt.Printf("circle area: %f", cval.Area())
+func (s Square) Area() float64 {
+	fmt.Print("Print square Area function")
+	return s.side * s.side
 }
+func (c Circle) Area() float64 {
+	fmt.Println("print circle area function")
+	return 3.14 * c.radius * c.radius
+}
+
+// func main() {
+// 	fmt.Println("Hello Welcome to this printInterface main function")
+// 	val := Square{side: 10.0}
+// 	fmt.Println("square val:", val.Area())
+
+// 	cval := Circle{radius: 5.0}
+// 	fmt.Printf("circle area: %f", cval.Area())
+// }
